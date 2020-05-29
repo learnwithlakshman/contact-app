65c1fefc118a2fc299bdc25bbd3209338e95ae8e
M Rajesh <m.rajeshrajz@gmail.com>
2020-05-29 12:11:48 +0530

    added Swagger UI

--- src/main/java/com/lwl/contactbook/ContactbookApplication.java
+++ src/main/java/com/lwl/contactbook/ContactbookApplication.java
@@ -9,23 +9,14 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class ContactbookApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ContactbookApplication.class, args);
-	}
-	
-	@Bean
-	public Docket productApi() {
-		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("com.lwl.contactbook")).build().apiInfo(metaData());
-	}
-
-	private ApiInfo metaData() {
-		return new ApiInfoBuilder().title("Contact Book API")
-				.description("Spring Boot REST API for CRUD Operations").version("1.0.0").build();
 	}
 
 }
--- src/main/java/com/lwl/contactbook/config/AppConfig.java
+++ src/main/java/com/lwl/contactbook/config/AppConfig.java
@@ -12,6 +12,15 @@ import springfox.documentation.spring.web.plugins.Docket;
 @Configuration
 public class AppConfig {
 	
-	
+	@Bean
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.lwl.contactbook")).build().apiInfo(metaData());
+	}
+
+	private ApiInfo metaData() {
+		return new ApiInfoBuilder().title("Contact Book API")
+				.description("Spring Boot REST API for CRUD Operations").version("1.0.0").build();
+	}
 
 }
